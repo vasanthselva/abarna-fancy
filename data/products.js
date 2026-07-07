3af5185d883226e00e69cc20fbaa6f2bf15be4a9
vasanth <123473435+vasanthselva@users.noreply.github.com>
2026-07-07 16:03:21 +0530

    Fix image filename for 'fevi quick' product

--- data/products.js
+++ data/products.js
@@ -332,7 +332,7 @@ export const products = [
     id: "vlr-045",
     name: "fevi quick",
     category: "Stationary",
-    image: "/images/fevi quicks.jpg ",
+    image: "/images/fevi quick.jpg ",
     likes: 220,
   },
    {
